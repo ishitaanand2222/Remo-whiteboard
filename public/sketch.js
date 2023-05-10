34fec49d9182d4a8e3d7fe891d8e96a8ccb68958
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-05-10 18:11:28 +0530

    first commit | whiteboard

---
+++ public/sketch.js
@@ -0,0 +1,89 @@
+var socket;
+var b;
+var c;
+
+const querystring = window.location.search;
+console.log(querystring);
+
+const urlParams = new URLSearchParams(querystring);
+
+const room = urlParams.get("key");
+var roomId = room;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight - 3.5);
+    background(255);
+    b = 0;
+
+    weight = document.getElementById('weight');
+    c = "#000000";
+
+    console.log("Weight ki value",weight.value);
+    if(weight.value < 1 && weight.value > 200){
+        alert("Enter value between 1 and 200 ONLY !");
+    }
+
+    socket = io()
+    socket.emit('create', roomId);
+    console.log(room);
+    socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+    b = data.display;
+    if (b == 1) {
+        background(255);
+        b = 0;
+    }
+    stroke(data.color);
+    // console.log("Data ka color",data.color);
+    // console.log("Data ka weight", data.weight);
+    strokeWeight(data.weight);
+    line(data.x, data.y, data.px, data.py);
+}
+
+function mouseDragged() {
+    //console.log(mouseX + ', ' + mouseY);
+    data = {
+        x: mouseX,
+        y: mouseY,
+        px: pmouseX,
+        py: pmouseY,
+        color: c,
+        weight: weight.value
+    }
+    socket.emit('mouse', data);
+    stroke(c);
+    // console.log(c);
+    strokeWeight(weight.value);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+}
+
+function resetSketch() {
+    background(255);
+    b = 1;
+    erase = {
+        display: b
+    }
+    socket.emit('mouse', erase);
+}
+
+function changered() {
+    c = "#f44336";
+    weight.value = 3;
+}
+
+function changeblue() {
+    c = "#008cba";
+    weight.value = 3;
+}
+
+function changeblack() {
+    c = "#000000";
+    weight.value = 3;
+}
+
+function Erase() {
+    c = "#ffffff";
+    weight.value = 30;
+}
